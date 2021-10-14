e94dbdfb2f2bbf50ab3b86b35c80abfca696e35b
Casper Yonel <yonelcasper@gmail.com>
2021-10-14 14:24:42 -0600

    2nd commit

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -8,6 +8,7 @@ var arr = [10,20,30,40,50,60]
 
 //Code Here
 
+let firstItem = arr[0]
 
 ////////// PROBLEM 2 //////////
 /*
@@ -15,6 +16,9 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+
+var lastItemRemoved = arr.pop()
+console.log(arr)
 
 ////////// PROBLEM 3 //////////
 
@@ -28,7 +32,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 
 //Code Here
 
-
+for (let i = 0; (i < family.length); i++){
+  console.log(family[i])
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -43,7 +49,12 @@ let evensArr = []
 
 //Code Here
 
-
+for (let i = 0; i < nums.length; i++){
+  if (nums[i] % 2 === 0) {
+    evensArr.push(nums[i])
+  }
+}
+console.log(evensArr)
 
 ////////// PROBLEM 5 //////////
 
